2fbeb26a0aae701197c78797781a1ee6c9681a42
Jay Batra <jaybatra73@gmail.com>
2019-02-13 13:38:55 +0530

    hbook/ntup: add example on how to create histos from CSV
    This CL adds example to create histogram from a
    CSV.
    
    Fixes go-hep/hep#186.
    
    Signed off by: Jay Batra <jaybatra73@gmail.com>

---
+++ hbook/ntup/ntup_example_test.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The go-hep Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ntup_test
+
+import (
+	"fmt"
+	"log"
+
+	"go-hep.org/x/hep/hbook/ntup/ntcsv"
+)
+
+func ExampleNtuple_scanH2D() {
+	nt, err := ntcsv.Open(
+		"testdata/simple-with-header.csv",
+		ntcsv.Comma(';'),
+		ntcsv.Header(),
+		ntcsv.Columns("v1", "v2", "v3"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer nt.DB().Close()
+
+	got, err := nt.ScanH2D("v1, v2", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("XMean:      %f\n", got.XMean())
+	fmt.Printf("YMean:      %f\n", got.YMean())
+	fmt.Printf("XRMS:       %f\n", got.XRMS())
+	fmt.Printf("YRMS:       %f\n", got.YRMS())
+	fmt.Printf("XStdDev:    %f\n", got.XStdDev())
+	fmt.Printf("YStdDev:    %f\n", got.YStdDev())
+	fmt.Printf("XStdErr:    %f\n", got.XStdErr())
+	fmt.Printf("YStdErr:    %f\n", got.YStdErr())
+
+	// Output:
+	// XMean:      4.500000
+	// YMean:      4.500000
+	// XRMS:       5.338539
+	// YRMS:       5.338539
+	// XStdDev:    3.027650
+	// YStdDev:    3.027650
+	// XStdErr:    0.957427
+	// YStdErr:    0.957427
+}
